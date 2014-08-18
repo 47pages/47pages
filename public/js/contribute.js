bc116b2639cbe1e5fd6d258885d9d70902c1fab3
Matt Dahl <matt.dahl.2013@gmail.com>
2014-08-17 20:30:11 -0400

    Adds client-side submission validation; refactors Contribute module.

--- public/js/contribute.js
+++ public/js/contribute.js
@@ -1,89 +1,156 @@
+// 47 Pages namespace
 var FSP = FSP || {};
 
-// Format the form based on the submission type (art vs. literature)
-FSP.format_form = {
-	toggle_literature: function () {
-		$('.submission-form__form--art-specific').each(function (index, element) {
-			$(element).hide();
+/**
+ * @module
+ * @description Methods to format and validate contribution form
+ */
+FSP.contribute = function () {
+	/**
+	 * @private
+	 */
+	var self = this,
+		format_form = {	// Format the form based on the submission type (art vs. literature)
+			toggle_literature: function () {
+				$('.submission-form__form--art-specific').each(function (index, element) {
+					$(element).hide();
+				});
+				$('.submission-form__form--literature-specific').each(function (index, element) {
+					$(element).show();
+				});
+			},
+			toggle_art: function () {
+				$('.submission-form__form--art-specific').each(function (index, element) {
+					$(element).show();
+				});
+				$('.submission-form__form--literature-specific').each(function (index, element) {
+					$(element).hide();
+				});
+			},
+			show_editing_options: function () {
+				$('.submission-form__form--editing-options').each(function (index, element) {
+					$(element).show();
+				});
+			},
+			hide_editing_options: function () {
+				$('.submission-form__form--editing-options').each(function (index, element) {
+					$(element).hide();
+				});
+			}
+		},
+		validate_form = function () {
+			if ($('input[name=author]').val() === '') {
+				alert('You must enter your name. It will remain confidential until publication.');
+				return false;
+			}
+			else if ($('input[name=contactEmail').val() === '') {
+				// Thorough regex validation happens at the Mongoose level
+				alert('You must enter your email address. It will remain confidential unless you opt-in to receiving edits.');
+				return false;
+			}
+			else if ($('input[name=submission').val() === '') {
+				alert('You must enter your submission.');
+				return false;
+			}
+			else if ($('input[name=title').val() === '') {
+				alert('You must enter the title of your piece.');
+				return false;
+			}
+			else if ($('input[name=submission').val() === '') {
+				alert('You must enter your submission.');
+				return false;
+			}
+			else if ($($('input[name=submissionType]'))[1].checked && $('textarea[name=technicalDetails]').val() === '') {
+				alert('You must enter your art piece\'s technical details.');
+				return false;
+			}
+			else {
+				return true;
+			}
+		};
+
+	/**
+	 * @public
+	 * @description Binds listeners to handle literature/art type selection and presentation of guidelines modal
+	 */
+	this.init = function () {
+		$('input[name="submissionType"]').change(function (event) {
+			if (event.currentTarget.value === 'literature') {
+				format_form.toggle_literature();
+			}
+			else if (event.currentTarget.value === 'art') {
+				format_form.toggle_art();
+			}
 		});
-		$('.submission-form__form--literature-specific').each(function (index, element) {
-			$(element).show();
+
+		$('input[name="willingToEdit"]').change(function (event) {
+			if (event.currentTarget.value === 'true') {
+				format_form.show_editing_options();
+			}
+			else if (event.currentTarget.value === 'false') {
+				format_form.hide_editing_options();
+			}
 		});
-	},
-	toggle_art: function () {
-		$('.submission-form__form--art-specific').each(function (index, element) {
-			$(element).show();
+
+		$('#show_submission_guidelines').on('click', function (event) {
+			event.stopPropagation();
+			self.show_submission_guidelines();
 		});
-		$('.submission-form__form--literature-specific').each(function (index, element) {
-			$(element).hide();
+	};
+
+	/**
+	 * @public
+	 * @description Shows the submission guidelines modal
+	 */
+	this.show_submission_guidelines = function () {
+		$('.submission-guidelines').show();
+		$('.fade-background').show();
+
+		// Disable scrolling in the background
+		$('body').addClass('no-scroll');
+
+		// Listen for exit
+		$('.submission-guidelines').on('click', function (event) {
+			event.stopPropagation(); // Don't exit if the click is within the modal
 		});
-	},
-	show_editing_options: function () {
-		$('.submission-form__form--editing-options').each(function (index, element) {
-			$(element).show();
+		$(window).on('click', function (event) {
+			self.hide_submission_guidelines(); // Anywhere else, exit
 		});
-	},
-	hide_editing_options: function () {
-		$('.submission-form__form--editing-options').each(function (index, element) {
-			$(element).hide();
+		$(document).keydown(function (event) {
+			if (event.which === 27) {
+				self.hide_submission_guidelines(); // Also exit on esc press
+			}
 		});
-	}
+	};
+
+	/**
+	 * @public
+	 * @description Hides the submission guidelines modal
+	 */
+	this.hide_submission_guidelines = function () {
+		$('.submission-guidelines').hide();
+		$('.fade-background').hide();
+
+		// Re-enable scrolling on the body
+		$('body').removeClass('no-scroll');
+
+		// Remove listener
+		$(window).off('click');
+	};
+
+	/**
+	 * @public
+	 * @description Submits the submission form after performing appropriate validation
+	 */
+	this.submit_submission = function () {
+		if (validate_form()) {
+			$('.submission-form__form').submit();
+		}
+	};
+
+	return this;
 };
 
-// Handle the modal display
-FSP.show_submission_guidelines = function () {
-	$('.submission-guidelines').show();
-	$('.fade-background').show();
+FSP.Contribute = FSP.contribute();
 
-	// Disable scrolling in the background
-	$('body').addClass('no-scroll');
-
-	// Listen for exit
-	$('.submission-guidelines').on('click', function (event) {
-		event.stopPropagation(); // Don't exit if the click is within the modal
-	});
-	$(window).on('click', function (event) {
-		FSP.hide_submission_guidelines(); // Anywhere else, exit
-	});
-	$(document).keydown(function (event) {
-		if (event.which === 27) {
-			FSP.hide_submission_guidelines(); // Also exit on esc press
-		}
-	});
-};
-
-FSP.hide_submission_guidelines = function () {
-	$('.submission-guidelines').hide();
-	$('.fade-background').hide();
-
-	// Re-enable scrolling on the body
-	$('body').removeClass('no-scroll');
-
-	// Remove listener
-	$(window).off('click');
-};
-
-$('document').ready(function () {
-	$('input[name="submissionType"]').change(function (event) {
-		if (event.currentTarget.value === 'literature') {
-			FSP.format_form.toggle_literature();
-		}
-		else if (event.currentTarget.value === 'art') {
-			FSP.format_form.toggle_art();
-		}
-	});
-
-	$('input[name="willingToEdit"]').change(function (event) {
-		if (event.currentTarget.value === 'true') {
-			FSP.format_form.show_editing_options();
-		}
-		else if (event.currentTarget.value === 'false') {
-			FSP.format_form.hide_editing_options();
-		}
-	});
-
-	$('#show_submission_guidelines').on('click', function (event) {
-		event.stopPropagation();
-		FSP.show_submission_guidelines();
-	});
-});
+$('document').ready(FSP.Contribute.init);
